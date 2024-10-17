147707ad6623a70ca7590fab44d8e79f450619f9
Pedro Galembeck <galembeckpedro@gmail.com>
2024-10-17 08:18:46 -0300

    refactor: component imports and update layout in FoodIntro component

--- src/app/food/components/food-intro.tsx
+++ src/app/food/components/food-intro.tsx
@@ -1,22 +1,26 @@
+import Image from "next/image";
+
 import { FlashFoodCard } from "@/components/food/flash-food-card";
 import { Button } from "@/components/ui/button";
-import { flashFood } from "@/constants";
+
+import { flashFood, howItWorks } from "@/constants";
+
 import { Pizza, Search } from "lucide-react";
-import Image from "next/image";
+import { HowItWorksCard } from "@/components/food/how-it-works-card";
 
 export const FoodIntro = () => {
   return (
     <div>
-      <section className="w-full h-[74rem] items-center justify-center relative -mt-14">
-        <div className="w-full h-[38.75rem] bg-[#FFBF0E] absolute" />
+      <section className="w-full items-center justify-center relative">
+        <div className="w-full h-[42rem] bg-[#FFBF0E] absolute max-[1120px]:h-[42rem] max-[1023px]:h-[35rem] max-[768px]:h-[32rem] max-[480px]:h-[30rem]" />
 
-        <div className="flex items-center justify-between gap-14 w-full max-w-[70rem] mr-auto ml-auto max-[1120px]:p-8 flex-col min-[900px]:flex-row relative">
+        <div className="flex items-center justify-between gap-14 w-full max-w-[70rem] mx-auto p-8 flex-col lg:flex-row relative lg:-mt-36 max-[1023px]:-mt-40 max-w-[640px]:mt-24 py-20 max-[639px]:-mt-14">
           <div>
             <div>
-              <h1 className="text-5xl font-baloo text-base-title font-extrabold mb-[1rem] sm:mt-20">
+              <h1 className="text-5xl font-baloo text-base-title font-extrabold mb-4 sm:mt-20 max-[768px]:text-3xl max-[480px]:text-2xl">
                 Encontre a refeição perfeita para qualquer hora do dia
               </h1>
-              <p className="text-base font-roboto text-base-subtitle font-normal text-pretty">
+              <p className="text-base font-roboto text-base-subtitle font-normal text-pretty max-[768px]:text-sm">
                 Com o serviço exclusivo de <strong>Food Delivery</strong> da{" "}
                 <strong>Wagon</strong>, você pode desfrutar de pratos saborosos
                 e frescos, onde quer que esteja, e a qualquer hora do dia, com
@@ -25,7 +29,7 @@ export const FoodIntro = () => {
                 <br />
                 Explore nossa <strong>
                   seleção de pratos
-                </strong> cuidadosamente <strong>preparadas</strong> por nosso
+                </strong> cuidadosamente <strong>preparados</strong> por nosso
                 restaurantes parceiros.
               </p>
 
@@ -51,15 +55,15 @@ export const FoodIntro = () => {
           </div>
 
           <Image
-            src="/assets/food/intro-2.png"
+            src="/assets/food/food-intro.png"
             alt="Food"
             width={476}
             height={476}
-            className="min-[1120px]:block hidden relative top-14"
+            className="lg:block hidden relative top-14"
           />
         </div>
 
-        <div className="flex flex-wrap justify-center items-center p-8 mt-20 sm:mt-36 max-[639px]:mt-44">
+        <div className="flex flex-wrap justify-center items-center p-8 mt-10">
           {flashFood.map((food, index) => (
             <FlashFoodCard
               key={index}
@@ -67,6 +71,22 @@ export const FoodIntro = () => {
               name={food.name}
               restaurant={food.restaurant}
               remainingDays={food.remainingDays}
+            />
+          ))}
+        </div>
+      </section>
+
+      <section className="w-full max-w-[70rem] mx-auto flex flex-col items-center justify-center mt-10">
+        <h1 className="text-center mb-8 font-baloo font-bold text-orange-500 text-title-lg">
+          Praticidade a alguns cliques
+        </h1>
+        <div className="flex flex-wrap justify-center items-center gap-8">
+          {howItWorks.map((item, index) => (
+            <HowItWorksCard
+              key={index}
+              image={item.image}
+              title={item.title}
+              description={item.description}
             />
           ))}
         </div>
